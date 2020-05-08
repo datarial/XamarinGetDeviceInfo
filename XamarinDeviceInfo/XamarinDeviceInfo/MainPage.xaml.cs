732a3bbe84b983ecea5414311d70f325c31d9d7b
Carlos Garcia R <carlos@datarial.com>
2020-05-08 14:44:34 -0700

    Carga Inicial Codigo

---
+++ XamarinDeviceInfo/XamarinDeviceInfo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Plugin.DeviceInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinDeviceInfo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var deviceIdResult = new Label();
+            var deviceId = CrossDeviceInfo.Current.Id;
+
+
+            deviceIdResult.Text = deviceId;
+
+            titleDeviceId.Text = "My device Id: " + "\n" + deviceIdResult.Text + "\n";
+            titleDeviceId.Text += "Name" + "\n" + CrossDeviceInfo.Current.DeviceName + "\n";
+            titleDeviceId.Text += "Model" + "\n" + CrossDeviceInfo.Current.Manufacturer + "." + CrossDeviceInfo.Current.Model + "\n";
+            titleDeviceId.Text += "Version" + "\n" + CrossDeviceInfo.Current.Version + "." + CrossDeviceInfo.Current.VersionNumber + "\n";
+            titleDeviceId.Text += "Platform" + "\n" + CrossDeviceInfo.Current.Platform;
+
+
+
+        }
+    }
+}
